9ded6222ae5b3814955d774e2e2f18bc64bf0255
AHMED BEN BRAHIM <SESA519849@se.com>
2019-07-15 11:44:38 +0200

    add recharts package and add a canvas for barcharts

--- userstatistics/frontend/src/components/stats/Stats.js
+++ userstatistics/frontend/src/components/stats/Stats.js
@@ -1,4 +1,15 @@
 import React, { Component, Fragment } from "react";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
+
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { getStats } from "../../actions/stats";
@@ -12,6 +23,56 @@ export class Stats extends Component {
     this.props.getStats();
   }
   render() {
+    const data = [
+      {
+        name: "Jan",
+        value: 4000
+      },
+      {
+        name: "Feb",
+        value: 3000
+      },
+      {
+        name: "Mar",
+        value: 2000
+      },
+      {
+        name: "Apr",
+        value: 2780
+      },
+      {
+        name: "May",
+        value: 1890
+      },
+      {
+        name: "Jun",
+        value: 2390
+      },
+      {
+        name: "Jul",
+        value: 2390
+      },
+      {
+        name: "Aug",
+        value: 3490
+      },
+      {
+        name: "Sept",
+        value: 3490
+      },
+      {
+        name: "Oct",
+        value: 3490
+      },
+      {
+        name: "Nov",
+        value: 3490
+      },
+      {
+        name: "Dec",
+        value: 3490
+      }
+    ];
     return (
       <Fragment>
         <h2>User informations</h2>
@@ -37,6 +98,36 @@ export class Stats extends Component {
             ))} */}
           </tbody>
         </table>
+        <div className="col-md-7 m-auto">
+          <div className="card card-body mt-5">
+            <i
+              className="text-center far fa-chart-bar fa-5x"
+              style={{ color: "#ffed00" }}
+            />
+            <h4 className="text-center">
+              Canvas For User Login Stats {"    "}
+              <span class="badge badge-danger">Data are not real </span>
+            </h4>
+
+            <BarChart
+              width={600}
+              height={300}
+              data={data}
+              margin={{
+                top: 20,
+                right: 60,
+                left: 20,
+                bottom: 5
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Bar dataKey="value" fill="#00b6ff" />
+            </BarChart>
+          </div>
+        </div>
       </Fragment>
     );
   }
